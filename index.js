2937659007db2abb9775dc6b0bb9bb444fd3d140
Instafluff <35773245+instafluff@users.noreply.github.com>
2019-12-27 15:28:31 -0800

    Floofy Name Overlay

--- index.js
+++ index.js
@@ -9,13 +9,24 @@ function getRandomInt( num ) {
   return Math.floor( num * Math.random() );
 }
 
+const ComfyWeb = require( "webwebweb" );
+ComfyWeb.APIs[ "/name" ] = ( qs ) => {
+  var name = adjectives[ getRandomInt( adjectives.length ) ] + " " + nouns[ getRandomInt( nouns.length ) ];
+  if( Math.random() < 0.25 ) {
+    name = adjectives[ getRandomInt( adjectives.length ) ] + " " + name;
+  }
+  ComfyJS.Say( "/me @" + qs.user + " is a " + name );
+  return { user: qs.user, name };
+}
+ComfyWeb.Run( 8023 );
+
 ComfyJS.onCommand = ( user, command, message ) => {
   if( command === "name" ) {
-    var name = adjectives[ getRandomInt( adjectives.length ) ] + " " + nouns[ getRandomInt( nouns.length ) ];
-    if( Math.random() < 0.25 ) {
-      name = adjectives[ getRandomInt( adjectives.length ) ] + " " + name;
-    }
-    ComfyJS.Say( "/me @" + user + " " + name );
+    // var name = adjectives[ getRandomInt( adjectives.length ) ] + " " + nouns[ getRandomInt( nouns.length ) ];
+    // if( Math.random() < 0.25 ) {
+    //   name = adjectives[ getRandomInt( adjectives.length ) ] + " " + name;
+    // }
+    // ComfyJS.Say( "/me @" + user + " " + name );
   }
 }
 ComfyJS.Init( process.env.TWITCHUSER, process.env.OAUTH );
